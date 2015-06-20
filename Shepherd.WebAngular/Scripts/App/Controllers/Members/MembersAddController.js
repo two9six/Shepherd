61cba1411296a4de13b035bc5f88c8381de90a39
jayson opena <jaysonopena@gmail.com>
2015-06-20 09:16:35 +0800

    Added Angular project
    Added Angular project

---
+++ Shepherd.WebAngular/Scripts/App/Controllers/Members/MembersAddController.js
@@ -0,0 +1,25 @@
+﻿(function () {
+
+    var app = angular.module("shepherdApp");
+
+    app.controller("MembersAddController", ["$scope", "$location", "MemberService", function ($scope, $location, MemberService) {
+
+        $scope.member = {};
+
+        $scope.submitForm = function () {
+            
+            $scope.$broadcast('show-errors-event');
+            if ($scope.memberForm.$invalid) {
+                return;
+            }
+            else {
+                console.log("Insert Member: ");
+                console.log($scope.member);
+                MemberService.insertMember($scope.member);
+                $location.path("/Members");
+            }
+
+        }
+    }]);
+
+}());
---
+++ Shepherd.WebAngular/Scripts/App/ShepherdApp.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+
+    var app = angular.module("shepherdApp", ["ngRoute", "ui.bootstrap"]);
+
+    app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+
+        $routeProvider.when("/Members", {
+            templateUrl: "/Templates/MembersIndex.html",
+            controller: "MembersIndexController"
+        })
+        .when("/Members/Add", {
+            templateUrl: "/Templates/MembersAdd.html",
+            controller: "MembersAddController"
+        })
+        .when("/Members/Details/:id", {
+            templateUrl: "/Templates/MembersDetail.html",
+            controller: "MembersDetailController"
+        })
+        .when("/", {
+            templateUrl: "/Templates/Index.html",
+            controller: "HomeController"
+        })
+        .otherwise({
+            redirectTo : "/"
+        });
+
+
+        $locationProvider.html5Mode(true);
+
+    }]);
+
+
+
+
+}());
