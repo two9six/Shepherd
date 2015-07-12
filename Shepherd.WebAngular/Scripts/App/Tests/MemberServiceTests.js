e2f1843139842da2c6a1981d355d2bb284ac3ae7
jayson opena <jaysonopena@gmail.com>
2015-07-12 20:58:57 +0800

    tried jasmine unit test

---
+++ Shepherd.WebAngular/Scripts/App/Tests/MemberServiceTests.js
@@ -0,0 +1,22 @@
+﻿
+describe("Testing the MemberService.js", function () {
+    it("Try mocking the MemberService", function () {
+
+        module(function ($provide) {
+            $provide.service('MemberService', function () {
+                this.getMemberById = jasmine.createSpy('getMemberById').and.callFake(function (id) {
+                    //a fake implementation
+                    return { Id: "1", FirstName: "Test", LastName: "Test", MiddleName: "Mock", BirthDate: "06/15/2016", DateBabtized: "06/15/2016" }
+                });
+            });
+        });
+
+        var mockMemberSvc;
+
+        inject(function (MemberService) {
+            mockMemberSvc = MemberService;
+        });
+
+        expect(mockMemberSvc.getMemberById(1).FirstName).toEqual("Test");
+    });
+});
