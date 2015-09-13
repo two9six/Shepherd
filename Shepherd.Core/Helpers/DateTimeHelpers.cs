c3f5a505fec528febce9f8fe9511a41277598fb5
jayson opena <jaysonopena@gmail.com>
2015-09-13 20:40:56 +0800

    Restored files
    Restored DateTimeHelpersTests.cs and DateTimeHelpers.cs

---
+++ Shepherd.Core/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shepherd.Core.Helpers
+{
+    public static class DateTimeHelpers
+    {
+        public static int ComputeAge(DateTime dateOfBirth)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - dateOfBirth.Year;
+
+            if (now < dateOfBirth.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
